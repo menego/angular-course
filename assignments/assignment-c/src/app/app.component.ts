e7b9586c3c40b5bcedf0453a4ba704579e89f78f
Nicola Meneghetti <nicola.meneghetti@lighting.com>
2018-06-01 10:28:53 +0200

    Add 3rd assignment exercise

---
+++ assignments/assignment-c/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  toggleDisplayP = true;
+  clickNumber = 0;
+  clickLogs = [];
+
+  constructor() {
+
+  }
+
+  switchPDisplay() {
+    this.toggleDisplayP = !this.toggleDisplayP;
+    this.clickLogs.push(++this.clickNumber);
+  }
+
+}
